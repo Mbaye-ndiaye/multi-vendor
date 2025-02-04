68909375d4425941524f311d699ededdfce71360
Mbaye Ndiaye <babacarndiay546@gmail.com>
2025-02-04 21:57:17 +0000

    deployer site

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { FaSearch, FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 
 const Header = () => {
+    const [dropdown, setDropdown] = useState(false)
   return (
     <header className="bg-white shadow-md p-4 flex justify-between items-center">
       <div className="relative w-1/3">
@@ -12,9 +16,27 @@ const Header = () => {
         <FaSearch className="absolute left-3 top-3 text-gray-500" />
       </div>
 
-      <div className="flex items-center space-x-4">
-        <span className="text-gray-700 font-medium">Mon Profile</span>
-        <FaUserCircle className="text-3xl text-gray-700 cursor-pointer" />
+      <div className="relative">
+        <div
+          className="flex items-center space-x-4 cursor-pointer"
+          onClick={() => setDropdown((prev) => !prev)}
+        >
+          <span className="text-gray-700 font-medium">Mon Profile</span>
+          <FaUserCircle className="text-3xl text-gray-700" />
+        </div>
+        {dropdown && (
+          <div className="absolute right-0 mt-5 bg-white border border-gray-200 rounded-md shadow-lg py-1 w-48">
+            <Link
+              to="/admin/profil"
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            >
+              Profile
+            </Link>
+            <button className="block w-full text-left px-4 py-2 text-sm font-bold text-gray-700 hover:bg-gray-100">
+              Déconnexion
+            </button>
+          </div>
+        )}
       </div>
     </header>
   );
--- src/pages/Dashbord.jsx
+++ src/pages/Dashbord.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Card from '../component/Card'
 import { FaInfoCircle } from "react-icons/fa";
+import { FaMoneyCheckDollar } from "react-icons/fa6";
 
 
 const Dashbord = () => {
   return (
     <div className='flex justify-between w-full'>
       <Card title="Commandes" value={10} icon={FaInfoCircle} />
-      <Card title="Commandes" value={10} icon={FaInfoCircle} />
+      <Card title="Revenue" value={10} icon={FaMoneyCheckDollar} />
       <Card title="Commandes" value={10} icon={FaInfoCircle} />
       <Card title="Nombre produit disponible" value={10} icon={FaInfoCircle} />
     </div>
