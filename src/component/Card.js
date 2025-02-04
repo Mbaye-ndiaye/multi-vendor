93624fc0e675230ef845f32991ac75d64571697d
Mbaye Ndiaye <babacarndiay546@gmail.com>
2025-02-04 22:03:05 +0000

    deployer le site

--- src/component/Card.js
+++ src/component/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({title, value, icon: Icon}) => {
   return (
-    <div className="p-4 flex items-center justify-between shadow-lg rounded-2xl bg-white w-80 gap-4">
+    <div className="p-4 space-y-1 flex items-center justify-between shadow-lg rounded-2xl bg-white w-80 gap-4">
       <div>
         <h3 className="text-lg font-semibold text-gray-700">{title}</h3>
         <p className="text-2xl font-bold text-gray-900">{value}</p>
--- src/pages/Dashbord.jsx
+++ src/pages/Dashbord.jsx
@@ -6,7 +6,7 @@ import { FaMoneyCheckDollar } from "react-icons/fa6";
 
 const Dashbord = () => {
   return (
-    <div className='flex justify-between w-full'>
+    <div className='flex justify-between space-y-4 flex-wrap w-full '>
       <Card title="Commandes" value={10} icon={FaInfoCircle} />
       <Card title="Revenue" value={10} icon={FaMoneyCheckDollar} />
       <Card title="Commandes" value={10} icon={FaInfoCircle} />
